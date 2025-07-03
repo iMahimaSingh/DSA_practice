9fb56f9fea95f8eaa79b4a9ebbb1d217cc816838
Mahima Singh <155384344+iMahimaSingh@users.noreply.github.com>
2025-07-04 01:33:14 +0530

    reformat the questions

--- TUF+/basic/pattern/pattern10.py
+++ TUF+/basic/pattern/pattern10.py
@@ -5,27 +5,15 @@ Pattern 10
 
 Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
 
-
-
 *
-
 **
-
 ***
-
 ****
-
 *****
-
 ****
-
 ***
-
 **
-
 *
-
-
 
 Print the pattern in the function given to you.
 """
--- TUF+/basic/pattern/pattern8.py
+++ TUF+/basic/pattern/pattern8.py
@@ -5,14 +5,11 @@ Pattern 8
 
 Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
 
-
-
 *********
  *******
   *****
    ***
     *
-
 
 Print the pattern in the function given to you.
 
